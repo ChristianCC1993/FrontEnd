2dc4ca4f2f3e77432fdebeba83a395d0e2c9ab2a
Christian Concha <christiancc1993@gmail.com>
2025-05-08 20:13:38 -0400

    Modifique todo

--- src/views/login.jsx
+++ src/views/login.jsx
@@ -4,50 +4,61 @@ import '../styles/login.css';
 
 function Login({ credentials, setCredentials, handleLogin }) {
   const navigate = useNavigate();
+
   useEffect(() => {
     setCredentials({ login: '', clave: '' });
   }, []);
-  
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleLogin();
+  };
+
   return (
     <div className="login-container">
       <div className="login-box">
         <h2>Login</h2>
-        <input autoComplete="off"
-          type="text"
-          placeholder="Usuario"
-          value={credentials.login}
-          onChange={(e) => setCredentials({ ...credentials, login: e.target.value })}
-        />
-        <input autoComplete="off"
-          type="password"
-          placeholder="Contraseña"
-          value={credentials.clave}
-          onChange={(e) => setCredentials({ ...credentials, clave: e.target.value })}
-        />
-        
-        <a
-          href="#"
-          className="forgot-password"
-          onClick={(e) => {
-            e.preventDefault();
-            navigate('/recuperar-clave');
-          }}
-        >
-          ¿Olvidó su contraseña?
-        </a>
 
-        <a
-          href="#"
-          className="forgot-password"
-          onClick={(e) => {
-            e.preventDefault();
-            navigate('/registro');
-          }}
-        >
-          Crear una cuenta
-        </a>
+        <form onSubmit={handleSubmit}>
+          <input
+            autoComplete="off"
+            type="text"
+            placeholder="Usuario"
+            value={credentials.login}
+            onChange={(e) => setCredentials({ ...credentials, login: e.target.value })}
+          />
+          <input
+            autoComplete="off"
+            type="password"
+            placeholder="Contraseña"
+            value={credentials.clave}
+            onChange={(e) => setCredentials({ ...credentials, clave: e.target.value })}
+          />
 
-        <button onClick={handleLogin}>Login</button>
+          <a
+            href="#"
+            className="forgot-password"
+            onClick={(e) => {
+              e.preventDefault();
+              navigate('/recuperar-clave');
+            }}
+          >
+            ¿Olvidó su contraseña?
+          </a>
+
+          <a
+            href="#"
+            className="forgot-password"
+            onClick={(e) => {
+              e.preventDefault();
+              navigate('/registro');
+            }}
+          >
+            Crear una cuenta
+          </a>
+
+          <button type="submit">Login</button>
+        </form>
       </div>
     </div>
   );
